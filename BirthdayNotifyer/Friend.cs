4cd3a2e2c3baf4b6606c83886add0a543a1a2692
Дмитрий Иванков <dimakill228@gmail.com>
2022-03-23 20:40:13 +0700

    Сделал тесты для методов класса "Друг". Реализовал сами методы.

--- BirthdayNotifyer/Friend.cs
+++ BirthdayNotifyer/Friend.cs
@@ -1,14 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BirthdayNotifyer
 {
     class Friend
     {
-        string name;
-        DateTime birthDate;
+        private string name;
+        private DateTime birthDate;
+
+        public Friend() 
+        {
+            
+        }
+
+        public Friend(string name, DateTime dt)
+        {
+            this.name = name;
+            this.birthDate = dt;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public DateTime GetBirthDate()
+        {
+            return birthDate;
+        }
     }
 }
---
+++ BirthdayNotifyer/FriendTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace BirthdayNotifyer
+{
+    class FriendTests
+    {
+        [Test]
+        public void CreateFriendNameEqual()
+        {
+            string name = "друг 1";
+            DateTime dt = new DateTime();
+            Friend friend = new Friend(name, dt);
+
+            Assert.AreEqual(name, friend.GetName());
+        }
+
+        [Test]
+        public void CreateFriendDateEqual()
+        {
+            string name = "друг 1";
+            DateTime dt = new DateTime(2000, 6, 13);
+            Friend friend = new Friend(name, dt);
+
+            Assert.AreEqual(dt, friend.GetBirthDate());
+        }
+    }
+}
