401b798512f58abd1de8b0965ed357f93d8343a6
Дмитрий Иванков <dimakill228@gmail.com>
2022-03-23 21:41:49 +0700

    Сделал новые тесты для методов класса "Записная книжка". Расширил функционал класса и провёл рефакторинг.

--- BirthdayNotifyer/Notebook.cs
+++ BirthdayNotifyer/Notebook.cs
@@ -19,7 +19,38 @@ namespace BirthdayNotifyer
 
         internal void AddFriend(string name, DateTime dateTime)
         {
-            friends.Add(new Friend());
+            if (FindFriend(name, dateTime))
+                return;
+
+            friends.Add(new Friend(name, dateTime));
+        }
+
+        internal void DeleteLastFriend()
+        {
+            friends.RemoveAt(friends.Count -1);
+        }
+
+        internal bool FindFriend(string name, DateTime bd)
+        {
+            for (int i = 0; i < friends.Count; i++)
+            {
+                if (friends[i].GetName() == name && friends[i].GetBirthDate() == bd)
+                    return true;
+            }
+
+            return false;
+        }
+
+        internal void DeleteFriend(string name, DateTime bd)
+        {
+            for (int i = 0; i < friends.Count; i++)
+            {
+                if (friends[i].GetName() == name && friends[i].GetBirthDate() == bd)
+                {
+                    friends.RemoveAt(i);
+                    return;
+                }
+            }
         }
     }
 }
--- BirthdayNotifyer/NotebookTests.cs
+++ BirthdayNotifyer/NotebookTests.cs
@@ -1,23 +1,121 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BirthdayNotifyer
 {
     class NotebookTests
     {
         [Test]
-        public void AddFriendToListIncreaseCount([Values(0, 1, 2, 3, 10, 15, 20)] int friendsCount)
+        public void AddFriendIncreaseCount([Values(0, 1, 2, 3, 10, 15, 20)] int friendsCount)
         {
             Notebook notebook = new Notebook();
 
             for (int i = 0; i < friendsCount; i++)
-                notebook.AddFriend("ФИО", new DateTime());
+                notebook.AddFriend("ФИО" + i.ToString(), new DateTime());
 
             Assert.AreEqual(friendsCount, notebook.GetFriendsCount());
+        }
+
+        [Test]
+        public void AddSameFriendDoNotRepeat()
+        {
+            Notebook notebook = new Notebook();
+            string name = "друг 1";
+            DateTime dt = new DateTime(2000, 6, 13);
+
+            notebook.AddFriend(name, dt);
+            notebook.AddFriend(name, dt);
+
+            Assert.AreEqual(1, notebook.GetFriendsCount());
+        }
+
+        [Test]
+        public void FindFriendCorrect()
+        {
+            Notebook notebook = new Notebook();
+            DateTime dt = new DateTime(2000, 6, 13);
+            string name = "друг 1";
+
+            notebook.AddFriend(name, dt);
+
+            Assert.AreEqual(true, notebook.FindFriend(name, dt));
+        }
+
+        [Test]
+        public void FindFriendIncorrectName()
+        {
+            Notebook notebook = new Notebook();
+            DateTime dt = new DateTime(2000, 6, 13);
+            string name = "друг 1";
+            string name2 = "друг 2";
+
+            notebook.AddFriend(name, dt);
+
+            Assert.AreEqual(false, notebook.FindFriend(name2, dt));
+        }
+
+        [Test]
+        public void FindFriendIncorrectBirthdate()
+        {
+            Notebook notebook = new Notebook();
+            DateTime dt = new DateTime(2000, 6, 13);
+            DateTime dt2 = new DateTime(2001, 6, 13);
+            string name = "друг 1";
+
+            notebook.AddFriend(name, dt);
+
+            Assert.AreEqual(false, notebook.FindFriend(name, dt2));
+        }
+
+        [Test]
+        public void DeleteLastFriendDecreaseCount([Values(5, 10)] int friendsCount, [Values(1, 2, 3, 5)] int deleteCount)
+        {
+            Notebook notebook = new Notebook();
+
+            for (int i = 0; i < friendsCount; i++)
+                notebook.AddFriend("ФИО" + i.ToString(), new DateTime());
+
+            for (int i = 0; i < deleteCount; i++)
+                notebook.DeleteLastFriend();
+
+            Assert.AreEqual(friendsCount - deleteCount, notebook.GetFriendsCount());
+        }
+
+        [Test]
+        public void DeleteFriendByNameAndBirthdateDecreaseCount()
+        {
+            Notebook notebook = new Notebook();
+            DateTime dt1 = new DateTime(2000, 6, 13);
+            DateTime dt2 = new DateTime(2001, 6, 13);
+            string name1 = "друг 1";
+            string name2 = "друг 2";
+
+            notebook.AddFriend(name1, dt1);
+            notebook.AddFriend(name2, dt2);
+
+            notebook.DeleteFriend(name1, dt1);
+
+            Assert.AreEqual(1, notebook.GetFriendsCount());
+        }
+
+        [Test]
+        public void DeleteFriendByNameAndBirthdateRemoveCorrectFriend()
+        {
+            Notebook notebook = new Notebook();
+            DateTime dt1 = new DateTime(2000, 6, 13);
+            DateTime dt2 = new DateTime(2001, 6, 13);
+            string name1 = "друг 1";
+            string name2 = "друг 2";
+
+            notebook.AddFriend(name1, dt1);
+            notebook.AddFriend(name1, dt2);
+            notebook.AddFriend(name2, dt1);
+            notebook.AddFriend(name2, dt2);
+
+            notebook.DeleteFriend(name1, dt2);
+
+            Assert.AreEqual(false, notebook.FindFriend(name1, dt2));
         }
     }
 }
